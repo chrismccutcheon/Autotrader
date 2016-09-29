34c11c7805e8154e0be55ac39f936c8063ef2cbf
Chris McCutcheon <cmccutcheon2015@gmail.com>
2016-09-29 11:28:46 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var yahooFinance = require('yahoo-finance');
+var _ = require('underscore');
+var moment = require('moment');
+var db = require('./db.js');
+
+var PORT = process.env.PORT || 3000;
+//moment().format();
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connect', function (socket) {
+	console.log('User connected via socket.io!');
+
+	socket.on('snapshot', function (symbolsString) {
+    console.log(symbolsString);
+		var symbols = symbolsString.symbol.split(' ');
+		var startDate = symbolsString.startDate;
+		var endDate = symbolsString.endDate;
+		console.log(startDate);
+    console.log(typeof(startDate));
+    yahooFinance.historical({
+      symbols: symbols,
+			from:startDate,
+			to: endDate,
+      //fields: ['s', 'n', 'd1', 'l1', 'y', 'r'],
+    }, function (err, data) {
+			var dataSeries = []
+			for(var i = 0; i < data[symbols[0]].length; i++){
+				//2016-01-29T08:00:00.000Z
+				var curdate = moment(data[symbols[0]][i].date).unix();
+				//curdate = curdate.unix();
+				dataSeries.push([ Number(curdate+"000"), data[symbols[0]][i].close]);
+			}
+			//console.log(dataSeries);
+      io.emit('data', {"title": symbols[0], "data": dataSeries});
+    });
+
+	});
+
+});
+
+app.post('/users', function(req, res){
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.create(body).then(function (user) {
+		res.json(user.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
+});
+
+db.sequelize.sync().then(function() {
+	http.listen(PORT, function(){
+  	console.log('listening on :' + PORT);
+	});
+});
---
+++ public/js/app.js
@@ -0,0 +1,65 @@
+var socket = io();
+
+socket.on('connect', function () {
+	console.log('Conncted to socket.io server!');
+});
+
+socket.on('snapshot', function (data) {
+	chart(data);
+
+	jQuery('.messages').append('<p>' + data + '<p>');
+});
+
+socket.on('data', function(data){
+  //console.log(data);
+	//for(var i = 0; i < data.length; ++i){
+	//	jQuery('.messages').append('<p>' + data[i] + '<p>')
+	//}
+	chart(data);
+});
+
+// Handle submiting of new message
+
+var $form = jQuery("#getHistorical");
+
+$form.on('submit', function(event){
+	event.preventDefault();
+	var $message = $form.find('input[name=symbol]');
+	var $start = $form.find('input[name=start]');
+	var $end = $form.find('input[name=end]');
+	console.log($message.val());
+	socket.emit('snapshot', {
+		symbol: $message.val(),
+		startDate: $start.val(),
+		endDate: $end.val()
+	});
+	$message.val('');
+	$start.val('');
+	$end.val('');
+});
+
+function chart (chartData) {
+		console.log(chartData);
+
+        // Create the chart
+        $('#container').highcharts('StockChart', {
+
+
+            rangeSelector: {
+                selected: 1
+            },
+
+            title: {
+                text:  chartData.title
+            },
+
+            series: [{
+                name: 'AAPL',
+                data: chartData.data,
+                tooltip: {
+                    valueDecimals: 2
+                }
+            }]
+        });
+
+}
